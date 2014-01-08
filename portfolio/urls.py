3519587e45cf0098388399776892390f4881dd74
Ian MacFarland <ian@Ians-MacBook-Pro.local>
2014-01-07 16:14:16 -0800

    bare views for portfolio and project

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from portfolio import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.home, name='home'),
+    url(r'^$', views.portfolio, name='portfolio'),
+    url(r'^project/(?P<slug>[\w-]+)/$', views.project, name="project"),
     url(r'^resume/$', views.resume, name='resume'),
 )
--- portfolio/views.py
+++ portfolio/views.py
@@ -4,10 +4,15 @@ from django.template import RequestContext
 from django.forms import ModelForm
 import models
 
-def home(request):
+def portfolio(request):
     project_list = models.Project.objects.all()
-    return render_to_response('portfolio/home.html', 
+    return render_to_response('portfolio/portfolio.html', 
         {'project_list' : project_list}, RequestContext(request))
+
+def project(request, slug):
+    project = models.Project.objects.get(slug=slug)
+    return render_to_response('portfolio/project.html', 
+        {'project' : project}, RequestContext(request))
     
 def resume(request):
     jobs = models.Job.objects.all()
