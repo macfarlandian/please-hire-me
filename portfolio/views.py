d940129efb7aa84492503f3c799cec9510a8d443
Ian MacFarland <ian@Ians-MacBook-Pro.local>
2014-01-08 17:55:34 -0800

    pull name from default header, send to all views

--- portfolio/views.py
+++ portfolio/views.py
@@ -4,23 +4,28 @@ from django.template import RequestContext
 from django.forms import ModelForm
 import models
 
+# your name, from header, for all views
+name = {}
+header = models.ResumeHeader.objects.get(pk=1)
+name['first'] = header.first_name
+name['last'] = header.last_name
+
 def portfolio(request):
     project_list = models.Project.objects.all()
     return render_to_response('portfolio/portfolio.html', 
-        {'project_list' : project_list}, RequestContext(request))
+        {'project_list' : project_list, 'name': name }, RequestContext(request))
 
 def project(request, slug):
     project = models.Project.objects.get(slug=slug)
     return render_to_response('portfolio/project.html', 
-        {'project' : project}, RequestContext(request))
+        {'project': project, 'name': name }, RequestContext(request))
 
 def role(request, slug):
     role = models.Role.objects.get(slug=slug)
     return render_to_response('portfolio/role.html', 
-        {'role' : role}, RequestContext(request))
+        {'role': role, 'name': name }, RequestContext(request))
 
 def resume(request):
-    content = {'header' : models.ResumeHeader.objects.get(pk=1)}
-    return render_to_response('portfolio/resume.html', content, 
-        RequestContext(request))
+    return render_to_response('portfolio/resume.html', 
+    {'header': header, 'name': name }, RequestContext(request))
     
