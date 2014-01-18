1d227525cbaf04e72b846e9ab151eabb904444b5
Ian MacFarland <ian@Ians-MacBook-Pro.local>
2014-01-17 18:01:43 -0800

    "name" object needs to be in each view or it doesn't query the db for changes

--- portfolio/views.py
+++ portfolio/views.py
@@ -5,30 +5,32 @@ from django.forms import ModelForm
 import models
 
 # your name, from header, for all views
-name = {}
-header = models.ResumeHeader.objects.get(pk=1)
-name['first'] = header.first_name
-name['last'] = header.last_name
-name['photo'] = header.photo
-name['w'] = header.img_width
-name['h'] = header.img_height
+def getname():
+    name = {}
+    header = models.ResumeHeader.objects.get(pk=1)
+    name['first'] = header.first_name
+    name['last'] = header.last_name
+    name['photo'] = header.photo
+    name['w'] = header.img_width
+    name['h'] = header.img_height
+    return name
 
 def portfolio(request):
     project_list = models.Project.objects.all()
     return render_to_response('portfolio/portfolio.html', 
-        {'project_list' : project_list, 'name': name }, RequestContext(request))
+        {'project_list' : project_list, 'name': getname() }, RequestContext(request))
 
 def project(request, slug):
     project = models.Project.objects.get(slug=slug)
     return render_to_response('portfolio/project.html', 
-        {'project': project, 'name': name }, RequestContext(request))
+        {'project': project, 'name': getname() }, RequestContext(request))
 
 def role(request, slug):
     role = models.Role.objects.get(slug=slug)
     return render_to_response('portfolio/role.html', 
-        {'role': role, 'name': name }, RequestContext(request))
+        {'role': role, 'name': getname() }, RequestContext(request))
 
 def resume(request):
     return render_to_response('portfolio/resume.html', 
-    {'header': header, 'name': name }, RequestContext(request))
+    {'header': header, 'name': getname() }, RequestContext(request))
     
